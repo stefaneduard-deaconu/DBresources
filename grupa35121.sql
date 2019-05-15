2d6470398242b1bb15e84c8254bd7ecf84238038
stefaneduard-deaconu <stefaneduard.deaconu@gmail.com>
2019-05-15 09:00:06 +0300

    first hour

--- grupa35121.sql
+++ grupa35121.sql
@@ -29,3 +29,19 @@ set job_id = 'SA_REP'
 where department_id = 80 and commission_pct is not null;
 
 --17
+
+update dep_sde
+set manager_id = (select employee_id from emp_sde
+                  where first_name || ' ' || last_name = 'Douglas Grant')
+where department_id = 20;
+
+select * from dep_sde where department_id = 20;
+rollback;
+
+--18
+
+update emp_sde
+set (salary, commission_pct) = (select mng.salary, mng.commission_pct
+                                from emp_sde mng, emp_sde emp
+                                where mng.employee_id = emp.manager_id
+                                      and emp.salary = (select min(salary) from emp_sde));
